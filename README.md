457e12033d9a2a6bd66373a5e8e2cff7c297dea1
KaloyanEnev <128579742+KaloyanEnev@users.noreply.github.com>
2024-12-18 18:57:09 +0200

    added comment delete functionalty

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Angular-project
-to do - add validation and error check, login register and so on !!!
+to do - add validation and error check, login register and so on !!!, fix buttons to be shown disabled until all required fields and error checks are done succ!!
--- themes/src/app/theme/current-theme/current-theme.component.ts
+++ themes/src/app/theme/current-theme/current-theme.component.ts
@@ -1,27 +1,54 @@
 import { Component } from '@angular/core';
 import { Theme } from '../../types/theme';
 import { ApiService } from '../../api.service';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { UserService } from '../../user/user.service';
+import { User } from '../../types/user';
 
 @Component({
   selector: 'app-current-theme',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './current-theme.component.html',
-  styleUrl: './current-theme.component.css'
+  styleUrl: './current-theme.component.css',
 })
 export class CurrentThemeComponent {
+  theme = {} as Theme;
+  constructor(
+    private userService: UserService,
+    private apiService: ApiService,
+    private router: ActivatedRoute,
+    private navRouter: Router
+  ) {}
 
-theme = {} as Theme
-constructor(private apiService : ApiService,private router : ActivatedRoute,private navRouter : Router){
+  userId: any = '';
+  ngOnInit(): void {
+    const id = this.router.snapshot.params['themeId'];
+    this.apiService.getSingleTheme(id).subscribe((t) => {
+      this.theme = t;
+     
+      
+      this.userId = this.userService.user?._id
+      console.log('userid',this.userId);
+      
+      
+    });
+  }
+  onDelete(themeId: string, postId: string): void {
+    this.apiService.deletePost(themeId, postId).subscribe({
+      next: (data) => {
+        // Remove the post from the local array
+        this.theme.posts = this.theme.posts.filter((post: any) => post._id !== postId);
+  
+        // Optionally, navigate to the same page to reflect changes in the URL
+        this.navRouter.navigate([`/themes/${themeId}`]);
+      },
+      error: (err) => {
+        console.error('Error deleting post:', err);
+      }
+    });
+  }
+    
+    
+  }
 
-
-}
-ngOnInit(): void {
-  const id = this.router.snapshot.params['themeId'];
-  this.apiService.getSingleTheme(id).subscribe((t) => {
-    this.theme = t;
-   
-  });
-}
-}
